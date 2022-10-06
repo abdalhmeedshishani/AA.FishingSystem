5a0dc8f121798517f1e916ee15fc3b2720b828f8
Venom <abdalhmeedshishani@gmail.com>
2022-10-07 00:27:58 +0300

    -Added AA.FishingSystem.EntityFrameworkCore class library   Added in EF Core -class ApplicationDbContext.cs -Inherited ApplicationDbContext.cs class with DbContext -Added in ApplicationDbContext.cs class thus code  public DbSet<>    { get; set; } and ApplicationDbContext : base(option) -Added ConnectionStrings to appsettings.json -Refrenced the AA.FishingSystem.EntityFrameworkCore class library with web -Installed SQL.Server and DbContext packges for both web and AA.FishingSystem.EntityFrameworkCore class library

--- AA.FishingSystem.Api/Program.cs
+++ AA.FishingSystem.Api/Program.cs
@@ -1,3 +1,6 @@
+using AA.FishingSystem.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace AA.FishingSystem.Api
 {
     public class Program
@@ -6,16 +9,16 @@ namespace AA.FishingSystem.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
---
+++ AA.FishingSystem.EntityFrameworkCore/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AA.FishingSystem.EntityFrameworkCore
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<>    { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+         
+
+    }
+}
